590ab3f414b6ad16ba7d57c5ce9443f8106632fa
Mahnoor Meher <mahnoorneher@gmail.com>
2025-06-30 15:58:25 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel= require("./models/Users")
+require('dotenv').config();
+
+
+const app=express();
+const port= process.env.PORT || 3001;
+app.use(cors());
+app.use(express.json())
+
+
+mongoose.connect(process.env.MONGO_URL)
+
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log("❌ MongoDB Connection Error:", err));
+
+
+app.get('/',(req,res)=>{
+    UserModel.find({})
+    .then(users =>res.json(users))
+    .catch(err =>res.json(err))
+})
+
+app.get('/getUser/:id' , (req,res)=>{
+    const id = req.params.id;
+    UserModel.findById({_id:id})
+    .then(users =>res.json(users))
+    .catch(err =>res.json(err))
+})
+
+app.put('/updateUser/:id',(req,res)=>{
+      const id = req.params.id;
+      UserModel.findByIdAndUpdate({_id:id},{name:req.body.name,email:req.body.email,age:req.body.age})
+       .then(users =>res.json(users))
+    .catch(err =>res.json(err))
+})
+
+app.delete('/deleteUser/:id',(req,res)=>{
+      const id = req.params.id;
+      UserModel.findByIdAndDelete({_id:id}) 
+       .then(res =>res.json(res))
+    .catch(err =>res.json(err))
+})
+
+app.post('/createUser',(req,res)=>{
+    UserModel.create(req.body)
+    .then(users=> res.json(users))
+    .catch(err => res.json(err))
+})
+
+
+
+app.listen(port,()=>{
+    console.log("Server is running")
+})
